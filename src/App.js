e63e1ea0de09a0e93737a671509adb24bfa3e375
Peter Remez <remez.p85@gmail.com>
2023-09-29 10:50:36 +0200

    first stylization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Router</h1>
+          <ul className='app-menu'>
+              <li className='app-item'><a href='/'>Home</a></li>
+              <li className='app-item'><a href='/people'>people</a></li>
+              <li className='app-item'><a href='/profile'>profile</a></li>
+              <li className='app-item'><a href='/some'>some</a></li>
+          </ul>
       </header>
+        <main className='app-main'>
+            Main
+        </main>
+        <footer>
+            Footer
+        </footer>
     </div>
   );
 }
